6d768adaf637d60433da88f434ad1596bd5eb412
LeeDongGeon1996 <secmatth1996@gmail.com>
2018-05-08 16:05:00 +0900

    2-2. Add Connector Class

---
+++ ST2M/src/main/java/kw/comso/st2m/MysqlConnector.java
@@ -0,0 +1,28 @@
+package kw.comso.st2m;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class MysqlConnector {
+
+	@Autowired
+	private transient static DataSource ds;
+	private static Connection connection = null;
+	
+	public static Connection getConnection() {
+		if(MysqlConnector.connection == null) {
+			try {
+				connection = ds.getConnection();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return connection;
+	}
+	
+}
