46c4f5952ccde1ad20036f6409e27a3e1f7a1b8f
matth <matth@DESKTOP-KTI11R0>
2018-05-10 14:41:50 +0900

    4-1.DB Connector 추가 정리

--- ST2M/src/main/java/kw/comso/dao/MysqlConnector.java
+++ ST2M/src/main/java/kw/comso/dao/MysqlConnector.java
@@ -9,7 +9,7 @@ import javax.sql.DataSource;
 /*
  * 현재는 싱글톤으로 구현했으나 
  * 나중에 pooling기능이 있는 dataSource클래스를 사용하여 커넥션풀을 이용할 수 있도록 개선해야함.
- * 아니면 JdbcTemplate의 사용을 고려해볼것.
+ * 그리고 JdbcTemplate의 사용을 고려해볼것.
  */
 public class MysqlConnector {
 
