909ad85731b896689c60e5c3b01be948b5cfdd4e
JunmanChoi <junman95@naver.com>
2018-06-05 18:02:57 +0900

    몽고디비 조작함수 JunmntestDAOImpl

--- ST2M/src/main/java/kw/comso/dao/JunmantestDAOImpl.java
+++ ST2M/src/main/java/kw/comso/dao/JunmantestDAOImpl.java
@@ -1,12 +1,17 @@
 package kw.comso.dao;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementSetter;
 
 import kw.comso.dto.MemberInfoVO;
-
+import kw.comso.dto.MemberVO;
 
 /*
  * 그냥 String형 변수에 sql을 작성하여 인자로 넘길지,
@@ -16,11 +21,22 @@ import kw.comso.dto.MemberInfoVO;
 public class JunmantestDAOImpl implements JunmantestDAO {
 
 	private MongoTemplate mongoTemplate;
- 
+
 	public void setMongoTemplate(MongoTemplate mongoTemplate) {
 		this.mongoTemplate = mongoTemplate;
 	}
-	
-	
-	
+
+	public boolean insertMember(final MemberInfoVO member) {
+
+		this.mongoTemplate.insert(member, "member");
+		
+		return true;
+	}
+
+	public boolean deleteMember(final MemberInfoVO member) {
+
+		this.mongoTemplate.remove(member, "member");
+		
+		return true;
+	}
 }
--- ST2M/src/main/java/kw/comso/dto/MemberInfoVO.java
+++ ST2M/src/main/java/kw/comso/dto/MemberInfoVO.java
@@ -3,7 +3,7 @@ package kw.comso.dto;
 import java.util.Calendar;
 
 //이름. id(이메일) , pw, 권한, 생년월일, {{지역}},{{전화번호}})
-<<<<<<< HEAD
+
 public class MemberInfoVO {
    String name;
    String email;
@@ -61,74 +61,3 @@ public class MemberInfoVO {
       return this.auth;
    }
 }
-=======
-public class MemberInfoVO {
-
-	String name;
-	String email;
-	String password;
-	Calendar birth;
-	String address;
-	String pNum;
-	String auth;
-
-	// setter
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public void setBirth(Calendar birth) {
-		this.birth = birth;
-	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
-	public void setPNum(String pNum) {
-		this.pNum = pNum;
-	}
-
-	public void setAuth(String auth) {
-		this.auth = auth;
-	}
-
-	// getter
-	public String getName() {
-		return this.name;
-	}
-
-	public String getEmail() {
-		return this.email;
-	}
-
-	public String getPassword() {
-		return this.password;
-	}
-
-	public Calendar getBirth() {
-		return this.birth;
-	}
-
-	public String getAddress() {
-		return this.address;
-	}
-
-	public String getPNum() {
-		return this.pNum;
-	}
-
-	public String getAuth() {
-		return this.auth;
-	}
-}
->>>>>>> branch 'JMC_after_mongo' of https://github.com/LeeDongGeon1996/ST2M2.git
