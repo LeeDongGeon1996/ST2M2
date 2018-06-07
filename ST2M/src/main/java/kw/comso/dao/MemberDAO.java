6c6ab107cacb06bdeb77b5ba4259653ca55cf0be
JunmanChoi <junman95@naver.com>
2018-06-07 17:37:16 +0900

    .

--- ST2M/src/main/java/kw/comso/dao/MemberDAO.java
+++ ST2M/src/main/java/kw/comso/dao/MemberDAO.java
@@ -6,10 +6,4 @@ public interface MemberDAO {
 	boolean insertMember(MemberVO member);
 	boolean updateMember(MemberVO member);
 	boolean deleteMember(MemberVO member);
-
-<<<<<<< HEAD
 }
- 
-=======
-}
->>>>>>> branch 'JMC_after_mongo' of https://github.com/LeeDongGeon1996/ST2M2.git
